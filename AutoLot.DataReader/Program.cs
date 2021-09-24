a46e3ab3d2c5958352aa14e8ad9683e0afc2fa1e
Ivan <ivan@ivan.ru>
2021-09-25 01:11:42 +0300

    simple DAL add

--- AutoLot.DataReader/Program.cs
+++ AutoLot.DataReader/Program.cs
@@ -18,15 +18,27 @@ using (SqlConnection connection = new SqlConnection())
     @"Select i.id, m.Name as Make, i.Color, i.Petname
         FROM Inventory i
         INNER JOIN Makes m on m.Id = i.MakeId";
+    sql += ";Select * from Customers;";
     SqlCommand myCommand = new SqlCommand(sql, connection);
+    SqlCommand testCommand = new SqlCommand();
+    testCommand.Connection = connection;
+    testCommand.CommandText = sql;
     // Obtain a data reader a la ExecuteReader().
     using (SqlDataReader myDataReader = myCommand.ExecuteReader())
     {
         // Loop over the results.
-        while (myDataReader.Read())
+        do
         {
-            Console.WriteLine($"-> Make: {myDataReader["Make"]}, PetName: {myDataReader["PetName"]}, Color: {myDataReader["Color"]}.");
-        }
+            while (myDataReader.Read())
+            {
+                for (int i = 0; i < myDataReader.FieldCount; i++)
+                {
+                    Console.Write(i != myDataReader.FieldCount - 1 ? $"{myDataReader.GetName(i)} = {myDataReader.GetValue(i)}, " : $"{myDataReader.GetName(i)} = {myDataReader.GetValue(i)} ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        } while (myDataReader.NextResult());
     }
 }
 Console.ReadLine();
