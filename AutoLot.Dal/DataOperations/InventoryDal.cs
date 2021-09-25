8da3e0f84af144c9217028aa5f5a7ff800e94485
Ivan MSI <arigatory@gmail.com>
2021-09-25 18:16:10 +0300

    transaction add

--- AutoLot.Dal/DataOperations/InventoryDal.cs
+++ AutoLot.Dal/DataOperations/InventoryDal.cs
@@ -48,13 +48,25 @@ namespace AutoLot.Dal.DataOperations
 
         public CarViewModel GetCar(int id)
         {
+
+            SqlParameter param = new SqlParameter
+            {
+                ParameterName = "@carId",
+                Value = id,
+                SqlDbType = SqlDbType.Int,
+                Direction = ParameterDirection.Input
+            };
             OpenConnection(); CarViewModel car = null;
             //This should use parameters for security reasons
-            string sql = $@"SELECT i.Id, i.Color, i.PetName,m.Name as Make FROM Inventory i INNER JOIN Makes m on m.Id = i.MakeId WHERE i.Id = {id}";
+            string sql = @"SELECT i.Id, i.Color, i.PetName,m.Name as Make
+                FROM Inventory i 
+                INNER JOIN Makes m on m.Id = i.MakeId 
+                WHERE i.Id = @carId";
             using SqlCommand command = new SqlCommand(sql, _sqlConnection)
             {
                 CommandType = CommandType.Text
             };
+            command.Parameters.Add(param);
             SqlDataReader dataReader = command.ExecuteReader(CommandBehavior.CloseConnection);
             while (dataReader.Read())
             {
@@ -80,30 +92,66 @@ namespace AutoLot.Dal.DataOperations
             }
             CloseConnection();
         }
-
-
         public void InsertAuto(Car car)
         {
-            OpenConnection(); // Format and execute SQL statement.
-            string sql = "Insert Into Inventory (MakeId, Color, PetName) Values " + $"('{car.MakeId}', '{car.Color}', '{car.PetName}')";
-            // Execute using our connection.
+            OpenConnection();
+            // Note the "placeholders" in the SQL query.
+            string sql = "Insert Into Inventory" +
+                "(MakeId, Color, PetName) Values" +
+                "(@MakeId, @Color, @PetName)";
+            // This command will have internal parameters.
             using (SqlCommand command = new SqlCommand(sql, _sqlConnection))
             {
-                command.CommandType = CommandType.Text; command.ExecuteNonQuery();
+                // Fill params collection.
+                SqlParameter parameter = new SqlParameter
+                {
+                    ParameterName = "@MakeId",
+                    Value = car.MakeId,
+                    SqlDbType = SqlDbType.Int,
+                    Direction = ParameterDirection.Input
+                };
+                command.Parameters.Add(parameter); parameter = new SqlParameter
+                {
+                    ParameterName = "@Color",
+                    Value = car.Color,
+                    SqlDbType = SqlDbType.NVarChar,
+                    Size = 50,
+                    Direction = ParameterDirection.Input
+                };
+                command.Parameters.Add(parameter); parameter = new SqlParameter
+                {
+                    ParameterName = "@PetName",
+                    Value = car.PetName,
+                    SqlDbType = SqlDbType.NVarChar,
+                    Size = 50,
+                    Direction = ParameterDirection.Input
+                };
+                command.Parameters.Add(parameter);
+                command.ExecuteNonQuery();
+                CloseConnection();
             }
-            CloseConnection();
         }
+
 
         public void DeleteCar(int id)
         {
             OpenConnection();
+            SqlParameter param = new SqlParameter
+            {
+                ParameterName = "@carId",
+                Value = id,
+                SqlDbType = SqlDbType.Int,
+                Direction = ParameterDirection.Input
+            };
             // Get ID of car to delete, then do so.
-            string sql = $"Delete from Inventory where Id = '{id}'";
+            string sql = "Delete from Inventory where Id = @carId";
             using (SqlCommand command = new SqlCommand(sql, _sqlConnection))
             {
                 try
                 {
-                    command.CommandType = CommandType.Text; command.ExecuteNonQuery();
+                    command.Parameters.Add(param);
+                    command.CommandType = CommandType.Text;
+                    command.ExecuteNonQuery();
                 }
                 catch (SqlException ex)
                 {
@@ -118,12 +166,156 @@ namespace AutoLot.Dal.DataOperations
         {
             OpenConnection();
             // Get ID of car to modify the pet name.
-            string sql = $"Update Inventory Set PetName = '{newPetName}' Where Id = '{id}'";
+
+            SqlParameter paramId = new SqlParameter
+            {
+                ParameterName = "@carId",
+                Value = id,
+                SqlDbType = SqlDbType.Int,
+                Direction = ParameterDirection.Input
+            };
+            SqlParameter paramName = new SqlParameter
+            {
+                ParameterName = "@petName",
+                Value = newPetName,
+                SqlDbType = SqlDbType.NVarChar,
+                Size = 50,
+                Direction = ParameterDirection.Input
+            };
+
+            string sql = "Update Inventory Set PetName = @petName Where Id = @carId";
             using (SqlCommand command = new SqlCommand(sql, _sqlConnection))
             {
+                command.Parameters.Add(paramId);
+                command.Parameters.Add(paramName);
                 command.ExecuteNonQuery();
             }
             CloseConnection();
+        }
+
+        public string LookUpPetName(int carId)
+        {
+            OpenConnection();
+            string carPetName;
+            // Establish name of stored proc.
+            using (SqlCommand command = new SqlCommand("GetPetName", _sqlConnection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+                // Input param.
+                SqlParameter param = new SqlParameter
+                {
+                    ParameterName = "@carId",
+                    SqlDbType = SqlDbType.Int,
+                    Value = carId,
+                    Direction = ParameterDirection.Input
+                };
+                command.Parameters.Add(param);
+                // Output param.
+                param = new SqlParameter
+                {
+                    ParameterName = "@petName",
+                    SqlDbType = SqlDbType.NVarChar,
+                    Size = 50,
+                    Direction = ParameterDirection.Output
+                };
+                command.Parameters.Add(param);
+                // Execute the stored proc.
+                command.ExecuteNonQuery();
+
+                // Return output param.
+                carPetName = (string)command.Parameters["@petName"].Value;
+                CloseConnection();
+            }
+            return carPetName;
+        }
+
+
+        public void ProcessCreditRisk(bool throwEx, int customerId)
+        {
+            OpenConnection();
+            // First, look up current name based on customer ID.
+            string fName;
+            string lName;
+            var cmdSelect = new SqlCommand("Select * from Customers where Id = @customerId", _sqlConnection);
+            SqlParameter paramId = new SqlParameter
+            {
+                ParameterName = "@customerId",
+                SqlDbType = SqlDbType.Int,
+                Value = customerId,
+                Direction = ParameterDirection.Input
+            };
+            cmdSelect.Parameters.Add(paramId);
+            using (var dataReader = cmdSelect.ExecuteReader())
+            {
+                if (dataReader.HasRows)
+                {
+                    dataReader.Read();
+                    fName = (string)dataReader["FirstName"];
+                    lName = (string)dataReader["LastName"];
+                }
+                else
+                {
+                    CloseConnection(); return;
+                }
+            }
+            cmdSelect.Parameters.Clear();
+            // Create command objects that represent each step of the operation.
+            var cmdUpdate = new SqlCommand("Update Customers set LastName = LastName + ' (CreditRisk) ' where Id = @customerId", _sqlConnection);
+            cmdUpdate.Parameters.Add(paramId);
+            var cmdInsert = new SqlCommand("Insert Into CreditRisks (CustomerId,FirstName, LastName) Values( @CustomerId, @ FirstName, @LastName)", _sqlConnection);
+            SqlParameter parameterId2 = new SqlParameter
+            {
+                ParameterName = "@CustomerId",
+                SqlDbType = SqlDbType.Int,
+                Value = customerId,
+                Direction = ParameterDirection.Input
+            };
+            SqlParameter parameterFirstName = new SqlParameter
+            {
+                ParameterName = "@FirstName",
+                Value = fName,
+                SqlDbType = SqlDbType.NVarChar,
+                Size = 50,
+                Direction = ParameterDirection.Input
+            };
+            SqlParameter parameterLastName = new SqlParameter
+            {
+                ParameterName = "@LastName",
+                Value = lName,
+                SqlDbType = SqlDbType.NVarChar,
+                Size = 50,
+                Direction = ParameterDirection.Input
+            };
+            cmdInsert.Parameters.Add(parameterId2);
+            cmdInsert.Parameters.Add(parameterFirstName);
+            cmdInsert.Parameters.Add(parameterLastName);
+            // We will get this from the connection object.
+            SqlTransaction tx = null; try
+            {
+                tx = _sqlConnection.BeginTransaction();
+                // Enlist the commands into this transaction.
+                cmdInsert.Transaction = tx; cmdUpdate.Transaction = tx;
+                // Execute the commands.
+                cmdInsert.ExecuteNonQuery(); cmdUpdate.ExecuteNonQuery();
+                // Simulate error.
+                if (throwEx)
+                {
+                    throw new Exception("Sorry!  Database error! Tx failed...");
+                }
+                // Commit it!
+                tx.Commit();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                //  Any error will roll back transaction.
+                //  Using the new conditional access operator to check for null.
+                tx?.Rollback();
+            }
+            finally
+            {
+                CloseConnection();
+            }
         }
 
         private void OpenConnection()
@@ -142,8 +334,6 @@ namespace AutoLot.Dal.DataOperations
                 _sqlConnection?.Close();
             }
         }
-
-
 
         bool _disposed = false;
         protected virtual void Dispose(bool disposing)
